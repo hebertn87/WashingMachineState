f6cf20e619c496a202ec76f85bc6197e86e9bcbe
Nate Hebert <hebertn87@gmail.com>
2018-03-07 09:26:33 -0700

    Pushed all files

---
+++ src/WashingMachine/State.java
@@ -0,0 +1,10 @@
+package WashingMachine;
+
+public class State {
+	public void insertQuarter();
+	public void ejectQuarter();
+	public void startLoad();
+	public void cancel();
+	
+	public void exception();
+}
---
+++ src/WashingMachine/hasPaid.java
@@ -0,0 +1,5 @@
+package WashingMachine;
+
+public class hasPaid {
+
+}
